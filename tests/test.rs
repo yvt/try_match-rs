2d64d62ab185843efd0307e7cf93b3c9b00890c0
yvt <i@yvt.jp>
2021-10-26 21:07:13 +0900

    Add tests
    I forgot why I had written these tests.

--- tests/test.rs
+++ tests/test.rs
@@ -28,4 +28,23 @@ fn input_evaled_only_once_implicit_map() {
 fn unwrap_option() {
     assert_eq!(try_match!(Some(a) = Some(42)), Ok(42));
     assert_eq!(try_match!(Some(a) = None::<u32>), Err(None));
+
+    let some = MyOption::Some(42);
+    let a = try_match!(MyOption::Some(a) = some);
+    assert_eq!(a, Ok(42));
+    assert_eq!(
+        try_match!(MyOption::Some(a) = MyOption::None),
+        Err(MyOption::None)
+    );
+    assert_eq!(try_match!(MyOption::Some(a) = some), Ok(42));
+    assert_eq!(
+        try_match!(MyOption::Some(a) = MyOption::None),
+        Err(MyOption::None)
+    );
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum MyOption {
+    Some(u32),
+    None,
 }
