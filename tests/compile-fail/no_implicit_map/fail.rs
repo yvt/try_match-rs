5ec61300c9354c26080beed85fa05793aca92e58
yvt <i@yvt.jp>
2023-05-02 09:12:01 +0900

    test: add a compile-fail test `no_implicit_map/fail`

---
+++ tests/compile-fail/no_implicit_map/fail.rs
@@ -0,0 +1,3 @@
+fn main() {
+    let _ = try_match::try_match!(Some((1, 2)), Some((_0, _1)));
+}
--- tests/compile.rs
+++ tests/compile.rs
@@ -3,3 +3,9 @@
 fn fail_tuple_captures() {
     trybuild::TestCases::new().compile_fail("tests/compile-fail/tuple_captures/**/*.rs");
 }
+
+#[test]
+#[cfg(not(feature = "implicit_map"))]
+fn fail_no_implicit_map() {
+    trybuild::TestCases::new().compile_fail("tests/compile-fail/no_implicit_map/**/*.rs");
+}
